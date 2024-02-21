2962a862c2491cdee7569d9077b77cb42324c20b
S-vz <s.vrzina@student.vu.nl>
2024-02-21 17:14:04 +0100

    test

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -18,7 +18,7 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-        # return add(body)
+        return add(body)
     return 'do some aaa!'
 
 
